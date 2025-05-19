0e4568e6d5c219d7d8a92390c2d04f9d72fa2dee
mreckah <m.reckahwalt@gmail.com>
2025-05-19 23:48:30 +0100

    updated portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: process.env.VITE_BASE_PATH || '/NABBAR-s-Portfolio',
+  base: './', // This ensures assets are loaded correctly in GitHub Pages
 });
